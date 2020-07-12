665f08beed3408954267cf3f4cb57d1b36f1ab75
dipesh shakya <dipeshshakya106@gmail.com>
2020-07-12 18:31:23 +1000

    featured room

--- src/Components/FeaturedRoom.js
+++ src/Components/FeaturedRoom.js
@@ -1,11 +1,24 @@
 import React, { Component } from "react";
 import { RoomContext } from "../context";
+import Loading from "./Loading";
+import Room from "./Room";
+import Title from "./Title";
 
 export default class FeaturedRoom extends Component {
   static contextType = RoomContext;
   render() {
-    const { featuredRoom: rooms } = this.context;
+    let { loading, featuredRoom: rooms } = this.context;
+    rooms = rooms.map((room) => {
+      return <Room key={room.id} room={room} />;
+    });
     console.log(rooms);
-    return <div>hello from featured room compoenent</div>;
+    return (
+      <section className="featured-rooms">
+        <Title title="featured rooms" />
+        <div className="featured-rooms-center">
+          {loading ? <Loading /> : rooms}
+        </div>
+      </section>
+    );
   }
 }
--- src/pages/Single.js
+++ src/pages/Single.js
@@ -1,8 +1,19 @@
 import React, { Component } from "react";
+import defaultbcg from "../images/room-1.jpeg";
+import { Link } from "react-router-dom";
+import { RoomContext } from "../context";
 
 export class Single extends Component {
+  constructor(props){
+    super(props)
+  }
+  componentDidMount(){
+    
+  }
   render() {
-    return <div>hllo from room page</div>;
+    return (
+      <seciton></seciton>
+    );
   }
 }
 
