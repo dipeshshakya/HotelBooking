7e4873021041ca572fe6d933780a684bfe71a566
dipesh shakya <dipeshshakya106@gmail.com>
2020-07-13 09:22:22 +1000

    higher order component

--- src/pages/Room.js
+++ src/pages/Room.js
@@ -2,16 +2,20 @@ import React from "react";
 import Hero from "../Components/Hero";
 import Banner from "../Components/Banner";
 import { Link } from "react-router-dom";
+import RoomContainer from "../Components/RoomContainer";
 
 function Room() {
   return (
-    <Hero hero="roomsHero">
-      <Banner title="our rooms">
-        <Link to="/" className="btn-primary">
-          Back home
-        </Link>
-      </Banner>
-    </Hero>
+    <>
+      <Hero hero="roomsHero">
+        <Banner title="our rooms">
+          <Link to="/" className="btn-primary">
+            Back home
+          </Link>
+        </Banner>
+      </Hero>
+      <RoomContainer />
+    </>
   );
 }
 
